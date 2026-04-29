43ab7a1eacb48db87251fc68737d4e8b20283d01
mongush_bk <mongush_bk@magnit.ru>
2026-04-29 12:22:55 +0300

    Fix crds for providerconfig

--- apis/namespaced/v1alpha1/providerconfigusage_types.go
+++ apis/namespaced/v1alpha1/providerconfigusage_types.go
@@ -29,7 +29,7 @@ import (
 // +kubebuilder:printcolumn:name="CONFIG-NAME",type="string",JSONPath=".providerConfigRef.name"
 // +kubebuilder:printcolumn:name="RESOURCE-KIND",type="string",JSONPath=".resourceRef.kind"
 // +kubebuilder:printcolumn:name="RESOURCE-NAME",type="string",JSONPath=".resourceRef.name"
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 type ProviderConfigUsage struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
